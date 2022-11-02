dcfe408e6063ec15c5e7a4a741248ba39b92b540
AndrewSigorskih <crescent8547@yandex.ru>
2022-11-02 17:32:51 +0400

    count_fastq: minor fixes

--- count_fastq.sh
+++ count_fastq.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # a simple script to count reads with identical ids in 2 files
-# args: folder to search files in
+# usage: ./count_fastq.sh DIRNAME
+# args: DIRNAME -- folder to search files in
 # filenames must be listed in a file "ids.txt" one per line; paired 
 # filenames must follow each other, e.g.
 # pair1_1
@@ -10,9 +11,11 @@
 # etc.
 if [ -z "$1" ]; then
         echo "Please specify input directory!"
+        name=`basename "$0"`
+        echo "Usage: ${name} DIRNAME"
         exit 101
 fi
-DIRNAME=$1
+DIRNAME=${1%/} 
 if [ ! -d $DIRNAME ]; then
         echo "path $DIRNAME does not exist!"
         exit 101
@@ -23,9 +26,9 @@ echo -e "id1\tid2\ttot_reads_1\tidentical" > $OUTFILENAME
 for (( idx = 0; idx < ${#names[@]}/2; idx++ )); do
         i=$((2*$idx))
         j=$((2*$idx + 1))
-        cnt1=(`grep -c ^@SRR "${DIRNAME}/${names[$i]}.fastq"`)
         grep ^@SRR "${DIRNAME}/${names[$i]}.fastq" | cut -f 1 -d " " | cut -d "." -f 2 > tmp1.tmp
         grep ^@SRR "${DIRNAME}/${names[$j]}.fastq" | cut -f 1 -d " " | cut -d "." -f 2 > tmp2.tmp
+        cnt1=(`wc -l < tmp1.tmp`)
         cnt2=(`awk 'FNR==NR{array[$0]; next} ($0 in array) { count++ } END { print count } ' tmp1.tmp tmp2.tmp`)
         echo -e "${names[$i]}\t${names[$j]}\t${cnt1}\t${cnt2}" >> $OUTFILENAME
         rm tmp1.tmp tmp2.tmp
