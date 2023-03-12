f39dcfd2a1537256d493a458d68386fc7223551b
AndrewSigorskih <crescent8547@yandex.ru>
2023-03-12 16:56:14 +0400

    deduplication stage added to stages.py

--- RnaChromProcessing/DataProcessors/Base.py
+++ RnaChromProcessing/DataProcessors/Base.py
@@ -3,7 +3,7 @@ from tempfile import TemporaryDirectory
 from typing import Any, Dict, List
 
 from stages import Dedup
-from ..utils import exit_with_error, run_command
+from ..utils import exit_with_error, make_directory, run_command
 from ..utils import dedup_default_cfg
 
 # https://stackoverflow.com/questions/74291040/running-multiple-external-commands-in-parallel-in-python
@@ -19,15 +19,18 @@ class BaseProcessor:
         self.base_dir: str = cfg.get('base_dir', os.getcwd())
         self.input_dir: str = cfg.get('input_dir', None)
         self.output_dir: str = cfg.get('input_dir', None)
-        self.rna_ids: List[str] = cfg.get('rna_ids', None)
-        self.dna_ids: List[str] = cfg.get('dna_ids', None)
+        self.dna_ids: List[str] = cfg.get('dna_ids', [])
+        self.rna_ids: List[str] = cfg.get('rna_ids', [])
+        self.keep: List[str] = cfg.get('keep', [])
         # spam errors
         self.validate_inputs()
         # create working directory
         self.work_dir = TemporaryDirectory(dir=self.base_dir)
         self.setup_dirs()
         # get stages-specific configs, maybe get default and update???
-        self.dupremover: Dedup = Dedup(cfg.get('dedup', dedup_default_cfg))
+        self.dupremover: Dedup = Dedup(cfg.get('dedup', dedup_default_cfg),
+                                       self.input_dir, self.dedup_dir)
+
     def validate_inputs(self):
         if not self.input_dir:
             exit_with_error('Input directory not specified!')
@@ -35,15 +38,25 @@ class BaseProcessor:
             exit_with_error('Output directory not specified!')
         if (not self.rna_ids) or (not self.dna_ids):
             exit_with_error('Input file ids not specified!')
+        if not self.keep:
+            exit_with_error('Empty keep list!')
+    
     def setup_dirs(self):
         self.dedup_dir: str = os.path.join(self.work_dir.name, 'dedup')
         self.rsite_dir: str = os.path.join(self.work_dir.name, 'rsites')
         self.trim_dir : str = os.path.join(self.work_dir.name, 'trim')
         self.hisat_dir: str = os.path.join(self.work_dir.name, 'hisat')
         self.bam_dir: str = os.path.join(self.work_dir.name, 'bam')
+        self.bed_dir: str = os.path.join(self.work_dir.name, 'bed')
+        self.contacts_dir: str = os.path.join(self.work_dir.name, 'contacts')
+        for dirname in (
+            self.dedup_dir, self.rsite_dir, self.trim_dir,
+            self.hisat_dir, self.bam_dir, self.bed_dir, self.contacts_dir
+        ):
+            make_directory(dirname)
 
     def run(self):
         os.chdir(self.workdir)
-        self.dupremover()
+        self.dupremover.run(self.dna_ids, self.rna_ids, self.cpus)
         ...
         self.chdir(self.base_dir)
--- RnaChromProcessing/DataProcessors/stages.py
+++ RnaChromProcessing/DataProcessors/stages.py
@@ -1,18 +1,78 @@
-from typing import Any, Dict, List
+import shutil
+import os
+
+from multiprocessing.pool import ThreadPool
+from tempfile import NamedTemporaryFile
+from typing import Any, Callable, Dict, List
 
 from ..utils import exit_with_error, run_command
 
-class Dedup:
-    def __init__(self, cfg: Dict[str, Any]):
+class BasicStage:
+    def __init__(self,
+                 input_dir: str,
+                 output_dir: str):
+        self.input_dir: str = input_dir
+        self.output_dir: str = output_dir
+
+class Dedup(BasicStage):
+    def __init__(self, 
+                 cfg: Dict[str, Any],
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.tool: str = cfg.get('tool', None)
         self.params: List[str] = cfg.get('params', [])
-        self.cpus = cfg.get('cpus', None)
+        self.tool_path: str = cfg.get('tool_path', shutil.which(self.tool))
+        self.cpus: int = cfg.get('cpus', None)
         if not self.tool:
             exit_with_error('Deduplication tool not specified!')
+        if not self.tool_path:
+            exit_with_error(f'Cannot deduce path to {self.tool} executable!')
 
-    def __call__(self, global_cpus: int):
+    def run(self,
+            dna_ids: List[str],
+            rna_ids: List[str],
+            global_cpus: int):
+        """Run chosen deduplication tool"""
+        if not self.cpus:
+            self.cpus = global_cpus
+        # get filenames
+        dna_files = (filename for filename in os.listdir(self.input_dir) 
+                     if any([filename.startswith(id) for id in dna_ids]))
+        rna_files = (filename for filename in os.listdir(self.input_dir) 
+                     if any([filename.startswith(id) for id in rna_ids]))
+        # choose tool to run
         if self.tool == 'skip':
-            pass
+            func: Callable[[str, str], None] = self._copy_files
+        elif self.tool == 'fastuniq':
+            func = self._run_fastuniq
+        else:  # unknown tool
+            exit_with_error('Unknown deduplication tool!')
+        # run chosen function
+        with ThreadPool(self.cpus) as pool:
+            pool.starmap(func, zip(dna_files, rna_files))
 
-    def _run_fastuniq(self):
-        pass
+    def _copy_files(self, dnafilename: str, rnafilename: str):
+        infile1 = os.path.join(self.input_dir, dnafilename)
+        infile2 = os.path.join(self.input_dir, rnafilename)
+        outfile1 = os.path.join(self.output_dir, dnafilename)
+        outfile2 = os.path.join(self.output_dir, rnafilename)
+        shutil.copy(infile1, outfile1)
+        shutil.copy(infile2, outfile2)
+
+    def _run_fastuniq(self,
+                      dnafilename: str,
+                      rnafilename: str):
+        """run fastuniq"""
+        infile1 = os.path.join(self.input_dir, dnafilename)
+        infile2 = os.path.join(self.input_dir, rnafilename)
+        outfile1 = os.path.join(self.output_dir, dnafilename)
+        outfile2 = os.path.join(self.output_dir, rnafilename)
+        if (infile1.endswith('.gz')) or (infile2.endswith('.gz')):
+            exit_with_error('Fastuniq tool does not accept gzipped files!')
+        with NamedTemporaryFile(mode='w', delete=False, dir='.') as f:
+            tmpfilename = f.name
+            print(infile1, infile2, sep='\n', file=f)
+        command = (f'{self.tool_path} -i {tmpfilename} '
+                    f'-o {outfile1} -p {outfile2}')
+        run_command(command, shell=True)
+        os.remove(tmpfilename)
