b4b4f9a1c038ce8e75d26eb5f541816d09f69030
AndrewSigorskih <crescent8547@yandex.ru>
2023-03-18 20:53:22 +0400

    added template for stats-calculating stage

--- RnaChromProcessing/DataProcessors/stages/basicstage.py
+++ RnaChromProcessing/DataProcessors/stages/basicstage.py
@@ -2,8 +2,6 @@ import shutil
 import os
 
 import concurrent.futures
-from multiprocessing.pool import ThreadPool
-from multiprocessing import Pool
 from typing import Callable, List
 
 from ...utils import exit_with_error
@@ -32,32 +30,6 @@ class BasicStage:
         shutil.copy(rna_in_file, rna_out_file)
         return 0
     
-    def run_function_pool(self,
-                          func: Callable[[str, str, str, str], int],
-                          dna_ids: List[str],
-                          rna_ids: List[str]):
-        # get filenames
-        filenames: List[str] = [x for x in os.listdir(self.input_dir)
-                                if not any([x.endswith(y) for y in suff_to_filter])]
-        dna_files = [find_in_list(id, filenames) for id in dna_ids]
-        rna_files = [find_in_list(id, filenames) for id in rna_ids]
-        dna_input_files = [os.path.join(self.input_dir, filename)
-                           for filename in dna_files]
-        rna_input_files = [os.path.join(self.input_dir, filename)
-                           for filename in rna_files]
-        dna_output_files = [os.path.join(self.output_dir, filename)
-                           for filename in dna_files]
-        rna_output_files = [os.path.join(self.output_dir, filename)
-                           for filename in rna_files]
-        # run func in parallel
-        #with ThreadPool(self.cpus) as pool:
-        with Pool(self.cpus) as pool:
-            results = pool.starmap(func, zip(dna_input_files, rna_input_files,
-                                   dna_output_files, rna_output_files))
-        if any([x != 0 for x in results]):
-            msg = f'One or several calls of {func.__name__} returned non-zero process exit code!'
-            exit_with_error(msg)
-    
     def run_function(self,
                      func: Callable[[str, str, str, str], int],
                      dna_ids: List[str],
@@ -75,8 +47,7 @@ class BasicStage:
                            for filename in dna_files]
         rna_output_files = [os.path.join(self.output_dir, filename)
                            for filename in rna_files]
-        # run func in parallel
-        # try https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
+        # run func in parallel (async)
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.cpus) as executor:
             futures = [executor.submit(func, dna_inp, rna_inp, dna_out, rna_out)
                        for dna_inp, rna_inp, dna_out, rna_out in 
---
+++ RnaChromProcessing/DataProcessors/stages/stats.py
@@ -0,0 +1,13 @@
+class StatsCalc:
+    def __init__(self, 
+                 work_dir: str,
+                 cpus: int):
+        self.work_dir: str = work_dir
+        self.cpus: int = cpus
+        
+
+    def count_fastqs():
+        pass
+
+    def count_bams():
+        pass
